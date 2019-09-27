4881c9b0e983503028a20f0dd3c411a9cf29d09c
Antoine-Goncalves <goncalves.devweb@gmail.com>
2019-09-27 15:26:42 +0200

    create constante and variable to script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 `use strict`;
 
+const buttonPlayer1 = document.querySelector(".player1");
+
+const buttonPlayer2 = document.querySelector(".player2");
+
+const buttonReset = document.querySelector(".reset");
+
+const input = document.querySelector("input");
+
+const resPlayer1 = document.querySelector("span");
+
+const resPlayer2 = document.querySelectorAll("span")[1];
+
+let scorePlayer1 = 0;
+
+let scorePlayer2 = 0;
+
 
