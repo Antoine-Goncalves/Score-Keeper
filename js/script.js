f9d3c545c0acb83a543aff5438af17229caaa72b
Antoine-Goncalves <goncalves.devweb@gmail.com>
2019-09-27 20:12:40 +0200

    create 2 functions for add score to player1 and player2 to script.js

--- js/script.js
+++ js/script.js
@@ -6,14 +6,34 @@ const buttonPlayer2 = document.querySelector(".player2");
 
 const buttonReset = document.querySelector(".reset");
 
-const input = document.querySelector("input");
+const numInput = document.querySelector("input");
 
-const resPlayer1 = document.querySelector("span");
+const scorePlayer1 = document.querySelector("span");
 
-const resPlayer2 = document.querySelectorAll("span")[1];
+const scorePlayer2 = document.querySelectorAll("span")[1];
 
-let scorePlayer1 = 0;
+const numFinalToWin = document.querySelectorAll("h2 span");
 
-let scorePlayer2 = 0;
+let goalPlayer1 = 0;
 
+let goalPlayer2 = 0;
 
+let scoreMax = 5;
+
+function addScoreForPlayer1() {
+	
+	goalPlayer1++;
+
+	scorePlayer1.textContent = goalPlayer1;
+
+}
+
+function addScoreForPlayer2() {
+	
+	goalPlayer2++;
+
+	scorePlayer2.textContent = goalPlayer2;
+
+}
+
+buttonPlayer1.addEventListener("click", addScoreForPlayer1);
